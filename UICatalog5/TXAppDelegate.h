7e7534cba90d21130cd1a647c467f154e7272a98
Tonny Xu <tonny.xu@gmail.com>
2012-05-20 14:51:19 +0900

    Initial Commit

---
+++ UICatalog5/TXAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TXAppDelegate.h
+//  UICatalog5
+//
+//  Created by Tonny Xu on 5/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TXAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UICatalog5/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UICatalog5
+//
+//  Created by Tonny Xu on 5/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TXAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([TXAppDelegate class]));
+  }
+}
